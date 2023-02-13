bcaf6a604b74af2362515dd27adf3b37b26c2518
S3JER <72788875+S3JER@users.noreply.github.com>
2023-02-13 11:41:08 +0100

    Camera view
    Creating the app with the view of the camera display.
    Can't rotate the app and the camera do not show the correct aspect ration
    
    Co-Authored-By: Mads Mogensen <mail@madsmogensen.dk>

--- app/src/main/java/dk/scuffed/whiteboardapp/MainActivity.kt
+++ app/src/main/java/dk/scuffed/whiteboardapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package dk.scuffed.whiteboardapp
 
+import android.hardware.Camera
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
+    private var mCamera: Camera? = null
+    private var mPreview: CameraPreview? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create an instance of Camera
+        mCamera = Camera.open()
+
+        mPreview = mCamera?.let {
+            // Create our Preview view
+            CameraPreview(this, it)
+        }
+
+        // Set the Preview view as the content of our activity.
+        mPreview?.also {
+            val preview: FrameLayout = findViewById(R.id.camera_preview)
+            preview.addView(it)
+        }
     }
 }
