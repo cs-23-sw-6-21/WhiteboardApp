71deca6cf3e39c3dfd214fcef7227b7e7ad4b1cb
Mads Mogensen <mail@madsmogensen.dk>
2023-03-01 13:18:20 +0100

    optimize framebuffer to bitmap stage

--- app/src/main/java/dk/scuffed/whiteboardapp/pipeline/stages/FramebufferToBitmapStage.kt
+++ app/src/main/java/dk/scuffed/whiteboardapp/pipeline/stages/FramebufferToBitmapStage.kt
@@ -5,17 +5,31 @@ import android.opengl.GLES20
 import android.util.Size
 import dk.scuffed.whiteboardapp.opengl.*
 import dk.scuffed.whiteboardapp.pipeline.*
+import java.nio.ByteBuffer
 
 internal class FramebufferToBitmapStage(private val inputFramebufferInfo: FramebufferInfo, config: Bitmap.Config, pipeline: Pipeline)
     : BitmapOutputStage(pipeline, Size(inputFramebufferInfo.textureSize.width, inputFramebufferInfo.textureSize.height), config) {
+
+    private val byteBuffer: ByteBuffer
+
+    init {
+        val width = inputFramebufferInfo.textureSize.width
+        val height = inputFramebufferInfo.textureSize.height
+        val bytesPerPixel = bytesPerPixel(inputFramebufferInfo.textureFormat)
+
+        byteBuffer = ByteBuffer.allocateDirect(width * height * bytesPerPixel)
+    }
 
     override fun update() {
         readFrameBuffer(outputBitmap, inputFramebufferInfo)
     }
 
     private fun readFrameBuffer(bitmap: Bitmap, framebufferInfo: FramebufferInfo) {
+        byteBuffer.position(0)
+
         glActiveTexture(inputFramebufferInfo.textureUnitPair.textureUnit)
-        val buffer = glReadPixels(0, 0, inputFramebufferInfo.textureSize.width, framebufferInfo.textureSize.height, GLES20.GL_RGBA)
-        bitmap.copyPixelsFromBuffer(buffer)
+        glReadPixels(0, 0, inputFramebufferInfo.textureSize.width, framebufferInfo.textureSize.height, GLES20.GL_RGBA, byteBuffer)
+
+        bitmap.copyPixelsFromBuffer(byteBuffer)
     }
 }
