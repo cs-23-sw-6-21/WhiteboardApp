012cfbc5743b8dec01c935958906cd8e0e97b327
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2023-03-02 11:54:15 +0100

    Mask out humans (#17)
    Masks out humans, only taking changes where there is not a human
    Also implemented a simple openCV dilation stage to use for this
    Also implemented a stage that can write to a framebuffer that can be used next frame

---
+++ app/src/main/java/dk/scuffed/whiteboardapp/pipeline/stages/StoreStage.kt
@@ -0,0 +1,31 @@
+package dk.scuffed.whiteboardapp.pipeline.stages
+
+import android.content.Context
+import android.opengl.GLES20
+import dk.scuffed.whiteboardapp.R
+import dk.scuffed.whiteboardapp.opengl.*
+import dk.scuffed.whiteboardapp.pipeline.FramebufferInfo
+import dk.scuffed.whiteboardapp.pipeline.GLOutputStage
+import dk.scuffed.whiteboardapp.pipeline.Pipeline
+
+internal class StoreStage(context: Context, private val inputFramebufferInfo: FramebufferInfo, private val outputFramebufferInfo: FramebufferInfo, pipeline: Pipeline) : GLOutputStage(context, R.raw.vertex_shader, R.raw.texture, pipeline) {
+    init {
+        setup()
+    }
+
+    override fun setupFramebufferInfo() {
+        frameBufferInfo = outputFramebufferInfo
+    }
+
+    override fun setupUniforms(program: Int) {
+        super.setupUniforms(program)
+
+        // We don't need the framebuffer resolution as it is the same as resolution :^)
+
+        // Input framebuffer
+        val framebufferTextureHandle = glGetUniformLocation(program, "source_texture")
+        glUniform1i(framebufferTextureHandle, inputFramebufferInfo.textureUnitPair.textureUnitIndex)
+        glActiveTexture(inputFramebufferInfo.textureUnitPair.textureUnit)
+        glBindTexture(GLES20.GL_TEXTURE_2D, inputFramebufferInfo.textureHandle)
+    }
+}
