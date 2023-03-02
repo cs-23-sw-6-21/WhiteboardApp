848e2d6beb214ca997374345d1564548418d6874
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2023-03-02 10:41:44 +0100

    Create DrawPipelineStage.kt

---
+++ app/src/main/java/dk/scuffed/whiteboardapp/pipeline/stages/DrawPipelineStage.kt
@@ -0,0 +1,111 @@
+package dk.scuffed.whiteboardapp.pipeline.stages
+
+import android.content.Context
+import android.opengl.GLES20
+import android.opengl.GLUtils
+import android.util.Log
+import android.util.Size
+import androidx.fragment.app.FragmentManager
+import dk.scuffed.whiteboardapp.R
+import dk.scuffed.whiteboardapp.opengl.*
+import dk.scuffed.whiteboardapp.pipeline.*
+import dk.scuffed.whiteboardapp.pipeline.FramebufferInfo
+import dk.scuffed.whiteboardapp.pipeline.GLOutputStage
+import dk.scuffed.whiteboardapp.pipeline.TextureUnitPair
+
+internal class DrawPipelineStage(
+    context: Context,
+    private val stages: MutableList<Stage>,
+    private val pipeline: Pipeline) : GLOutputStage(context, R.raw.vertex_shader, R.raw.passthrough_shader, pipeline)
+{
+
+    // TODO Get this from the view.
+    private val resolution: Size = Size(1080, 1920)
+
+    private var currentStageIndex: Int
+
+    // Framebuffer used for drawing bitmaps into to throw into the GLOutput
+    private var bitmapFramebuffer: FramebufferInfo
+
+    init {
+        setup()
+        currentStageIndex = stages.lastIndex
+        bitmapFramebuffer = allocateBitmapTexture()
+    }
+
+    override fun setupFramebufferInfo() {
+        // Set framebuffer to screen
+        frameBufferInfo = FramebufferInfo(0, 0, TextureUnitPair(0, 0), 0, resolution)
+    }
+
+    /// Go to the next stage in the pipeline, looping if reaching the end.
+    fun nextStage() {
+        currentStageIndex++
+        if (currentStageIndex > stages.lastIndex) {
+            currentStageIndex = 0
+        }
+        // Cant show itself!
+        if (stages[currentStageIndex] == this) {
+            nextStage()
+        }
+    }
+
+    /// Go to the previous stage in the pipeline, looping if reaching the end.
+    fun prevStage() {
+        currentStageIndex--
+        if (currentStageIndex < 0) {
+            currentStageIndex = stages.lastIndex
+        }
+        // Cant show itself!
+        if (stages[currentStageIndex] == this) {
+            prevStage()
+        }
+    }
+
+    override fun setupUniforms(program: Int) {
+        super.setupUniforms(program)
+
+
+        var inputFrameBufferInfo: FramebufferInfo? = null
+        val currentStage = stages[currentStageIndex]
+        if (currentStage is GLOutputStage) {
+            inputFrameBufferInfo = currentStage.frameBufferInfo
+        }
+        else if (currentStage is BitmapOutputStage) {
+            glActiveTexture(bitmapFramebuffer.textureUnitPair.textureUnit)
+            glBindTexture(GLES20.GL_TEXTURE_2D, bitmapFramebuffer.textureHandle)
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, currentStage.outputBitmap, 0)
+            inputFrameBufferInfo = bitmapFramebuffer
+        }
+        else {
+            Log.d("dsf", "This stage does not support debug viewing")
+        }
+
+        if (inputFrameBufferInfo != null) {
+            // Input framebuffer resolution
+            val framebufferResolutionHandle = glGetUniformLocation(program, "framebuffer_resolution")
+            glUniform2f(framebufferResolutionHandle, inputFrameBufferInfo.textureSize.width.toFloat(), inputFrameBufferInfo.textureSize.height.toFloat())
+
+            // Input framebuffer
+            val framebufferTextureHandle = glGetUniformLocation(program, "framebuffer")
+            glUniform1i(framebufferTextureHandle, inputFrameBufferInfo.textureUnitPair.textureUnitIndex)
+            glActiveTexture(inputFrameBufferInfo.textureUnitPair.textureUnit)
+            glBindTexture(GLES20.GL_TEXTURE_2D, inputFrameBufferInfo.textureHandle)
+        }
+    }
+
+    private fun allocateBitmapTexture() : FramebufferInfo{
+        val textureUnitPair = pipeline.allocateTextureUnit(this)
+        glActiveTexture(textureUnitPair.textureUnit)
+
+        val textureHandle = glGenTexture()
+        glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle)
+        glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR)
+        glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR)
+        glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE)
+        glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE)
+        glBindTexture(GLES20.GL_TEXTURE_2D, 0)
+        return FramebufferInfo(999, textureHandle, textureUnitPair, GLES20.GL_RGBA, Size(resolution.width, resolution.height))
+    }
+
+}
