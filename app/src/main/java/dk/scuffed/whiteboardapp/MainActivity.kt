54662153bf12adcdab06df88e2ba16d038f26d49
S3JER <72788875+S3JER@users.noreply.github.com>
2023-03-17 13:40:20 +0100

    Button View
    Created a view with a button which can be seen on the phone's screen.

--- app/src/main/java/dk/scuffed/whiteboardapp/MainActivity.kt
+++ app/src/main/java/dk/scuffed/whiteboardapp/MainActivity.kt
@@ -3,8 +3,12 @@ package dk.scuffed.whiteboardapp
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import dk.scuffed.whiteboardapp.opengl.OpenGLView
 import org.opencv.android.OpenCVLoader
 
@@ -25,7 +29,12 @@ class MainActivity : AppCompatActivity() {
             requestPermissions(arrayOf(android.Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST_CODE)
         }
         else {
-            setContentView(OpenGLView(this))
+            setContentView(R.layout.activity_main)
+            val buttonView = findViewById<ConstraintLayout>(R.id.button_view)
+            buttonView.addView(OpenGLView(this))
+            findViewById<Button>(R.id.round_button).setOnClickListener {
+                Log.d("BUTTONS", "User tapped the Supabutton")
+            }
         }
     }
 
