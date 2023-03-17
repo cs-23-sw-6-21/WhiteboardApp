fa116874445b1c7eb1f3a23009cab1118fdb2baa
Mads Mogensen <mail@madsmogensen.dk>
2023-03-17 13:00:53 +0100

    Fix biggest square
    We could not use the convex hull algorithm as it would get stuck if
    we did not have a convex polygon.
    
    Instead we find the square by taking the shortest distance to the next
    point until we have all our points.
    We use this to calculate the area of the quad and we then find the
    biggest quad from all possible quads.
    
    The quad is then fixed so it can be used in perspective correction.
    This is done by taking the most top left corner and then finding the most
    bottom left corner and so on and so forth.
    
    This gives us a quad that starts in the top left corner and which points
    has an anti-clockwise direction.

--- app/src/main/java/dk/scuffed/whiteboardapp/pipeline/Utils.kt
+++ app/src/main/java/dk/scuffed/whiteboardapp/pipeline/Utils.kt
@@ -2,6 +2,8 @@ package dk.scuffed.whiteboardapp.pipeline
 
 import android.content.Context
 import com.google.common.io.ByteStreams
+import dk.scuffed.whiteboardapp.utils.Vec2Float
+import kotlin.math.abs
 
 fun readRawResource(context: Context, resourceId: Int): String {
     val stream =
@@ -10,4 +12,9 @@ fun readRawResource(context: Context, resourceId: Int): String {
     stream.close()
 
     return string
+}
+
+// https://www.mathopenref.com/coordtrianglearea.html
+fun areaOfTriangle(a: Vec2Float, b: Vec2Float, c: Vec2Float): Float {
+    return abs((a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y)) / 2.0f)
 }
