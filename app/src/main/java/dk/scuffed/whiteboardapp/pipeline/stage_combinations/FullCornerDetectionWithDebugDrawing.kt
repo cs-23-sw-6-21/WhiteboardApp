808b32deb9b8282146a7056821a1a735b92d7d33
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2023-03-27 10:50:18 +0200

    Use new values everywhere

--- app/src/main/java/dk/scuffed/whiteboardapp/pipeline/stage_combinations/FullCornerDetectionWithDebugDrawing.kt
+++ app/src/main/java/dk/scuffed/whiteboardapp/pipeline/stage_combinations/FullCornerDetectionWithDebugDrawing.kt
@@ -39,9 +39,13 @@ internal fun fullCornerDetectionWithDebugDrawing(
 
     val openCVLineDetectionStage = OpenCVLineDetectionStage(
         edgesBitmapStage,
-        150,
-        200,
-        pipeline
+        325,
+        10,
+        pipeline,
+        3.0,
+        Math.PI / 135,
+        100.0,
+        Math.PI / 15
     )
 
     val verticalLinesAngleDiscriminatorStage = LinesAngleDiscriminatorStage(
