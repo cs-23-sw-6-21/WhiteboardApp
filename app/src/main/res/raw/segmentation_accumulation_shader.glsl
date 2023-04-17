703d499c31c90b8987562fee79b3b9221dde905f
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2023-04-17 10:15:04 +0200

    Made segmentation accumulation more readable

--- app/src/main/res/raw/segmentation_accumulation_shader.glsl
+++ app/src/main/res/raw/segmentation_accumulation_shader.glsl
@@ -11,14 +11,15 @@ uniform sampler2D oldAccumulator;
 void main() {
     vec2 samplersUV = gl_FragCoord.xy / resolution;
 
-    vec4 col1 = texture2D(segmentation, samplersUV);
-    vec4 col2 = texture2D(oldAccumulator, samplersUV);
+    vec4 segInput = texture2D(segmentation, samplersUV);
+    vec4 oldAccumulator = texture2D(oldAccumulator, samplersUV);
 
 
-    float segmentMap = 1.0 - step(0.05, col1.z);
+    // get the segment map as a 0 1 mask.
+    float inverseSegmentMap = 1.0 - step(0.05, segInput.z);
 
-
-    float add = (col2.x + accumulation_factor) * segmentMap;
+    // Set pixels to zero where it is segmented. Otherwise, increment pixels back towards to 1.
+    float add = (oldAccumulator.x + accumulation_factor) * inverseSegmentMap;
 
 
     // annoying hack because the segmentor outputs a slightly blue color for no reason at all.
