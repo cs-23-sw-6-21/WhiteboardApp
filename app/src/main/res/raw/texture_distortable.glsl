a0463c4c90d92b66b093a09f43b2997e19d18d74
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2023-05-08 10:12:30 +0200

    Update masking_shader.glsl (#86)
    * Update masking_shader.glsl
    
    * Removed unused calculations

--- app/src/main/res/raw/texture_distortable.glsl
+++ app/src/main/res/raw/texture_distortable.glsl
@@ -8,8 +8,6 @@ uniform sampler2D source_texture;
 varying vec3 v_TexCoordinate;
 
 void main() {
-    vec2 uv = v_TexCoordinate.xy / resolution;
-
     // Divides by Z to revert the multiplication of z done per vertex
     vec4 color = texture2D(source_texture, vec2(v_TexCoordinate.x / v_TexCoordinate.z, v_TexCoordinate.y / v_TexCoordinate.z));
 
