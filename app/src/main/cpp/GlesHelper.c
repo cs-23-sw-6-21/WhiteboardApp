a282958b74f133bec50787fdc1a7523558a7bc33
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2023-05-08 09:42:33 +0200

    made the gleshelper hack a little cleaner

--- app/src/main/cpp/GlesHelper.c
+++ app/src/main/cpp/GlesHelper.c
@@ -3,10 +3,9 @@
 JNIEXPORT void JNICALL
 
 
-// Change
+// This is required to allow calling glReadPixels with these parameters, because the Java bindings doesnt have this function signature for some reason
 Java_dk_scuffed_whiteboardapp_helper_GlesHelper_glReadPixels(JNIEnv *env, jobject instance, jint x,
                                                         jint y, jint width, jint height,
                                                         jint format, jint type) {
-    // TODO
     glReadPixels(x, y, width, height, format, type, 0);
 }
--- app/src/main/java/dk/scuffed/whiteboardapp/helper/GlesHelper.java
+++ app/src/main/java/dk/scuffed/whiteboardapp/helper/GlesHelper.java
@@ -5,7 +5,7 @@ import android.util.Log;
 public class GlesHelper
 {
     static {
-        Log.d("asd", "Loading libray!");
+        Log.d("asd", "Loading the GLESHelper!");
         System.loadLibrary("native-lib");
     }
     public static native void glReadPixels(int x, int y, int width, int height, int format, int type);
